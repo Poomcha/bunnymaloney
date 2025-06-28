8507ba1ec813d471b9a711d99660acdc6c5b4456
poomcha <ben.degeneve@gmail.com>
2025-06-28 19:19:13 +0200

    v1

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   const animationImages = document.querySelectorAll(".animimg");
   let state = 0;
+  let first = true;
   const animate = (animationImages) => {
     setInterval(() => {
-      animationImages.forEach((image, index) => {
-        if (index !== state) {
-          image.classList.add("hidden");
-        }
-      });
+      if (!first) {
+        const prev = state === 0 ? 7 : state - 1;
+        animationImages[prev].classList.toggle("hidden");
+      }
       animationImages[state].classList.toggle("hidden");
-      state = (state + 1) % 7;
+      first = false;
+      state = (state + 1) % 8;
     }, 100);
   };
   animate(animationImages);
